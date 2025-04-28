4b51f7f5019e4480bccf27bf36dfc665bc4d17a5
AndsonUfersa <141687115+Andyn-1307@users.noreply.github.com>
2025-04-28 08:28:55 -0300

    Implementação de projeto base da Q6.

---
+++ QuestoesIniciais/Questao6/Q6.C
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main (){
+    int matrizA[3][3], matrizB[3][3], matrizSoma[3][3];
+    int i, j;
+    // Declaração de matrizes 3x3 para armazenar os elementos inteiros.
+    // A matrizA e matrizB armazenarão os elementos digitados pelo usuário,
+    printf("Digite os elementos da matriz A (3x3):\n");
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matrizA[i][j]);
+        }
+    }
+    printf("Digite os elementos da matriz B (3x3):\n");
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matrizB[i][j]);
+        }
+    }
+    // A matrizSoma ira mostrar a soma das matrizes A e B.
+    printf("Soma das matrizes A e B:\n");
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+
+            printf("%d ", matrizSoma[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return(0);
+}
